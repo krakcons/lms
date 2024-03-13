9eb13f220d11491008b03d42aed5b295c6eb72b1
Billy Hawkes <billyhawkes02@gmail.com>
2024-03-13 16:32:07 -0400

    Removed ppr

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const withNextIntl = require("next-intl/plugin")();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	experimental: {
-		ppr: true,
-	},
+	// experimental: {
+	// 	ppr: true,
+	// },
 };
 
 module.exports = withNextIntl(nextConfig);
